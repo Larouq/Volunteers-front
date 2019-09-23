143ce0211cae42956cc18f6c62006e24968bd1d4
Benoit Rouquié <rouquie.b@gmail.com>
2019-09-23 16:59:25 +0200

    add config js

---
+++ src/config.js
@@ -0,0 +1,6 @@
+const config = {
+    YANDEX_API : "2e846d9f-4492-4c1d-b01a-fcb4810766c7",
+    MAPBOX_API: "pk.eyJ1IjoibGFyb3VxIiwiYSI6ImNqdWkydHVlNDBkajE0Mm43amViYTc5cmMifQ.wORRPu3OalU4jRYfUBJ01A"
+}
+
+export default config
